fb6d6eebba26b35b756f0657154af6bdfed4b9b5
Yen Tran <yentt1905@gmail.com>
2017-08-12 16:48:23 -0500

    Connected view-client-rest API

--- AppClient/src/main/java/edu/mum/client/controller/UserController.java
+++ AppClient/src/main/java/edu/mum/client/controller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
 	public String home(Model model) {
 		
 		 User p = uService.getUser("598e7ea5fb3cd11dac9d4189");
-		
+	
 		model.addAttribute("user",p);
 		return "usertest";
 	}
