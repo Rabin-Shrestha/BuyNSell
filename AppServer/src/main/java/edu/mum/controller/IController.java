12f2973d38046bf631905258bbedceb2cff4eb64
Prasanna <prasanna.bajracharya@gmail.com>
2017-08-12 11:27:37 -0500

    Comment module added with CRUD operation completed

--- AppServer/src/main/java/edu/mum/controller/IController.java
+++ AppServer/src/main/java/edu/mum/controller/IController.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface IController<T>{
     T add(T t);
     T update(T t, String id);
-    T delete(T t,String id);
+    T delete(T t, String id);
     List<T> getAll();
     T get(String id);
 }
