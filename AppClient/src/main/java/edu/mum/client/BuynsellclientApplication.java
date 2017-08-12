fe098ee84bf1a62dc233e4c98a2cdc3735795471
RabinShrestha <lovingspirit617@gmail.com>
2017-08-12 12:17:36 -0500

    basic client project structure defined

--- AppClient/src/main/java/edu/mum/client/BuynsellclientApplication.java
+++ AppClient/src/main/java/edu/mum/client/BuynsellclientApplication.java
@@ -1,12 +1,19 @@
 package edu.mum.client;
 
+import edu.mum.client.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class BuynsellclientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuynsellclientApplication.class, args);
+
 	}
 }
---
+++ AppClient/src/main/java/edu/mum/client/controller/UserController.java
@@ -0,0 +1,13 @@
+package edu.mum.client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Rabin Shrestha on 8/12/2017.
+ */
+@Controller
+public class UserController {
+   // UserService userService;
+
+}
