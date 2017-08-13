207b040c522327a9557ade2a48a96941de99bc1b
Prasanna <prasanna.bajracharya@gmail.com>
2017-08-13 09:36:22 -0500

    all models synched in client and ws consumption started

--- AppClient/src/main/java/edu/mum/client/controller/UserController.java
+++ AppClient/src/main/java/edu/mum/client/controller/UserController.java
@@ -5,25 +5,29 @@ import edu.mum.client.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Rabin Shrestha on 8/12/2017.
  */
 @Controller
+@RequestMapping("/user")
 public class UserController {
     // UserService userService;
 
     @Autowired
     private UserService uService;
 
-    @RequestMapping("/test")
-    public String home(Model model) {
+    @RequestMapping("/{id}")
+    public String home(Model model, @PathVariable String id) {
 
-        User p = uService.getUser("598e7ea5fb3cd11dac9d4189");
+        User user = uService.getUser(id);
 
-        model.addAttribute("user", p);
-        return "usertest";
+        System.out.println("returned data :"+user);
+
+        model.addAttribute("user", user);
+        return "userProfile";
     }
 
 }
